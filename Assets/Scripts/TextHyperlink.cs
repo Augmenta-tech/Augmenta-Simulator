7a260ff6e91505436d9ff04421c1cd8f430758c0
BenoitArbelot <benoit.arbelot@gmail.com>
2020-03-27 18:48:37 +0100

    Added About section in help. Updated README.

---
+++ Assets/Scripts/TextHyperlink.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TextHyperlink : MonoBehaviour, IPointerClickHandler
+{
+    public Canvas canvas;
+    public TMP_Text text;
+
+    private new Camera camera;
+
+    void Awake() {
+        // Get a reference to the camera if Canvas Render Mode is not ScreenSpace Overlay.
+        if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+            camera = null;
+        else
+            camera = canvas.worldCamera;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+
+        // Check if Mouse intersects any words and if so assign a random color to that word.
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, camera);
+
+        if (linkIndex != -1) {
+
+            Application.OpenURL("https://www.augmenta-tech.com");
+
+        }
+    }
+}
