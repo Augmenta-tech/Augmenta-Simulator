013e4779ecf9a4c617a244bc3e4196f4594b2916
paulinelvne <pauline.lavainne@insa-lyon.fr>
2023-03-10 14:38:14 +0100

    Remove Node Simulator

--- Assets/Scripts/Controllables/OSCManagerControllable.cs
+++ Assets/Scripts/Controllables/OSCManagerControllable.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class OSCManagerControllable : Controllable
 {
-    [Header("YO VERSION")]
-    [OSCProperty(isInteractible = false)] public int yoVersion;
-    [OSCProperty(isInteractible = false)] public int yoPort;
-
     [Header("OUTPUT SETTINGS")]
     [OSCProperty]
     public int outputPort;
     [OSCProperty]
     public string outputIP;
-    [OSCProperty]
-    [Tooltip("Time in s before a connection without heartbeat is deleted. 0 = Never.")]
-    public float connectionTimeout;
 }
